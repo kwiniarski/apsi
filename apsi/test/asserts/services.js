f5b8043afe3f33b4fac9bee4d078ed4e39ed660a
kwiniarski <winiarski.krzysztof@gmail.com>
2014-12-20 13:06:28 +0100

    Remove apsi directory from index

--- apsi/test/asserts/services.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-var chai = require('chai');
-var utils = require('chai/lib/chai/utils');
-
-utils.addMethod(chai.Assertion.prototype, 'path', function (str) {
-  new chai.Assertion(this._obj).to.be.equal(str.replace(/\//g, path.sep));
-});
