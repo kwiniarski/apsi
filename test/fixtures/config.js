05dd3c10971e20da6751b412639c29b0e6e94d35
kwiniarski <winiarski.krzysztof@gmail.com>
2015-01-11 19:02:33 +0100

    Updated path to services directories to match config.

--- test/fixtures/config.js
+++ test/fixtures/config.js
@@ -3,9 +3,8 @@ var path = require('path');
 module.exports = {
   BASE_DIR: path.normalize('/app'),
   APSI_DIR: path.normalize('/app/node_modules/app'),
-  APPLICATION_MODELS_DIR: path.normalize('/app/api/models'),
-  APPLICATION_SERVICES_DIR: path.normalize('/app/api/services'),
-  APPLICATION_SERVICES_CONFIG_DIR: path.normalize('/app/config/services'),
+  SERVICES_DIR: path.normalize('/app/api/services'),
+  SERVICES_CONFIG_DIR: path.normalize('/app/config/services'),
   CONTROLLERS_DIR: path.normalize('/app/api/controllers'),
   CONTROLLERS_CONFIG: path.normalize('/app/config/controllers'),
   EVENTS_LOG_CONFIG: path.normalize('/app/config/events-log'),
