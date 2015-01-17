b071a0e5d2dd1573392bfa04943775ee4d478ead
kwiniarski <winiarski.krzysztof@gmail.com>
2015-01-17 12:21:31 +0100

    Example controller file

--- test/example/api/controllers/users.js
+++ test/example/api/controllers/users.js
@@ -6,9 +6,40 @@
 
 'use strict';
 
+var surf = require('../../../../lib/server');
+var RequestError = require('../../../../lib/errors').RequestError;
+var _ = require('lodash');
 
 module.exports = {
-  find: function () {},
-  listAvatarImages: function () {},
-  addAvatarImage: function () {}
+  find: function (req, res, next) {
+    surf.models.users.find({
+      where: {
+        email: req.params[0]
+      }
+    }).done(function (err, data) {
+      if (err) {
+        return next(RequestError.BadRequest(err));
+      }
+      if (_.isEmpty(data)) {
+        return next(RequestError.NotFound());
+      }
+      return res.ok(data);
+    });
+  },
+  listAvatarImages: function (req, res) {
+    surf.models.users.findAll({
+      attributes: ['avatar']
+    }).then(res.ok);
+  },
+  addAvatarImage: function (res, req) {
+    surf.models.users.update({
+      avatar: req.body.image
+    }, {
+      where: {
+        id: req.param('id')
+      }
+    }).then(function(){
+      return surf.models.users.find(req.param('id'));
+    }).then(res.ok);
+  }
 };
