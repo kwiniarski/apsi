cc6714db8338443046f7c09b7bee2373ea210301
Krzysztof Winiarski <winiarski.krzysztof@gmail.com>
2016-01-02 14:38:34 +0100

    chore: remove Grunt and replace Blanket with Istanbul

---
+++ scripts/codeclimate.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+CODECLIMATE_FILE=codeclimate.txt
+CODECLIMATE_BIN=node_modules/codeclimate-test-reporter/bin/codeclimate.js
+NODEJS_BIN=/usr/bin/node
+COVERAGE_FILE=.coverage/lcov.info
+
+if [[ -z "$CODECLIMATE_REPO_TOKEN" ]]; then
+  if [[ -f $CODECLIMATE_FILE ]]; then
+    CODECLIMATE_REPO_TOKEN=$(<$CODECLIMATE_FILE)
+  else
+    echo "CodeClimate token not found!"
+    exit 1;
+  fi
+fi
+
+bash -c "CODECLIMATE_REPO_TOKEN=$CODECLIMATE_REPO_TOKEN $NODEJS_BIN $CODECLIMATE_BIN < $COVERAGE_FILE";
+exit 0;
+
